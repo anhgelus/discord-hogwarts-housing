d775dfb763af29e25a327485a5b1e7259df1e22a
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-08-17 21:44:11 +0200

    feat discord client

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var token = os.Args[1]
+
+func main() {
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		panic(err)
+	}
+
+	discord.Identify.Intents = discordgo.IntentsGuilds
+
+	err = discord.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	err = discord.UpdateGameStatus(0, "Je mange des jeans")
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	err = discord.Close()
+	if err != nil {
+		panic(err)
+	}
+}
