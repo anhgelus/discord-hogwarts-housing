547fe30784d342a6b18c640b14029b4d7e893f4d
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2022-08-17 22:57:33 +0200

    feat redis connection

--- src/database/redisManager.go
+++ src/database/redisManager.go
@@ -1,1 +1,16 @@
 package database
+
+import "github.com/go-redis/redis"
+
+func GetRedisClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		return nil
+	}
+	return client
+}
